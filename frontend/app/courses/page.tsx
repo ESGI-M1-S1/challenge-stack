2a8605aea84830f2c30c5649b13b84e66a4157da
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-03-05 09:30:32 +0100

    datatable to the moon

---
+++ frontend/app/courses/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Image from "next/image";
+
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import { UserNav } from "./components/user-nav";
+
+// Simulate a database read for tasks.
+async function getTasks() {
+  return [
+    {
+      id: "TASK-8782",
+      title:
+        "You can't compress the program without quantifying the open-source SSD pixel!",
+      status: "in progress",
+      label: "documentation",
+      priority: "medium",
+    },
+    {
+      id: "TASK-7878",
+      title:
+        "Try to calculate the EXE feed, maybe it will index the multi-byte pixel!",
+      status: "backlog",
+      label: "documentation",
+      priority: "medium",
+    },
+    {
+      id: "TASK-7839",
+      title: "We need to bypass the neural TCP card!",
+      status: "todo",
+      label: "bug",
+      priority: "high",
+    },
+    {
+      id: "TASK-5562",
+      title:
+        "The SAS interface is down, bypass the open-source pixel so we can back up the PNG bandwidth!",
+      status: "backlog",
+      label: "feature",
+      priority: "medium",
+    },
+    {
+      id: "TASK-8686",
+      title:
+        "I'll parse the wireless SSL protocol, that should driver the API panel!",
+      status: "canceled",
+      label: "feature",
+      priority: "medium",
+    },
+    {
+      id: "TASK-1280",
+      title:
+        "Use the digital TLS panel, then you can transmit the haptic system!",
+      status: "done",
+      label: "bug",
+      priority: "high",
+    },
+  ];
+}
+
+export default async function TaskPage() {
+  const tasks = await getTasks();
+
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
+}
