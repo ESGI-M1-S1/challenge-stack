8afbfe737960caa50f8f74540004802810ad0705
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-03-05 11:55:29 +0100

    menu bar + debut de chat

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,13 +2,12 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
-
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,10 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-        "min-h-screen bg-background font-sans antialiased",
-        fontSans.variable
-        )}>{children}</body>
+      <body
+        className={cn(
+          "h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
