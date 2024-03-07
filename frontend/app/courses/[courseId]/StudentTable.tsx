83d7d7aae7cf18655f0501de0a52fc80157ed587
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-03-07 16:39:47 +0100

    table

---
+++ frontend/app/courses/[courseId]/StudentTable.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Checkbox } from "@/components/ui/checkbox";
+
+const StudentsTable = ({ students }: { students: any }) => {
+  const [selectedStudents, setselectedStudents] = React.useState<string[]>([]);
+  useEffect(() => {
+    console.log("Selected courses", selectedStudents);
+  }, [selectedStudents]);
+  return (
+    <Table>
+      <TableCaption>A list of your courses to assign</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Select</TableHead>
+          <TableHead>Nom élève</TableHead>
+          <TableHead>E-mail</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {students?.map((student: any) => {
+          return (
+            <TableRow key={student.id}>
+              <TableCell>
+                <Checkbox
+                  onCheckedChange={(checked) => {
+                    if (!checked) {
+                      setselectedStudents(
+                        selectedStudents.filter((id) => {
+                          return id !== student.id;
+                        }),
+                      );
+                      return;
+                    }
+                    setselectedStudents([...selectedStudents, student.id]);
+                  }}
+                />
+              </TableCell>
+              <TableCell>{student.nom}</TableCell>
+              <TableCell>{student.email}</TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default StudentsTable;
--- frontend/app/courses/[courseId]/page.tsx
+++ frontend/app/courses/[courseId]/page.tsx
@@ -2,19 +2,15 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table";
 
 import { getAllStudents, getCoursById } from "@/modules/courses";
+import StudentsTable from "@/app/courses/[courseId]/StudentTable";
 
-const Page = async ({ params: { courseId } }: { params: { courseId: string } }) => {
+const Page = async ({
+  params: { courseId },
+}: {
+  params: { courseId: string };
+}) => {
   const course = await getCoursById(courseId);
   const students = await getAllStudents();
   console.log(students);
@@ -26,42 +22,19 @@ const Page = async ({ params: { courseId } }: { params: { courseId: string } }) 
     <div className="container">
       <header className={"pt-5 w-full flex justify-between"}>
         <h1 className={"text-4xl font-bold"}>{course.matiere}</h1>
-        <h4 className={"font-bold"}>Date du cours : {formattedStartDate} - {formattedEndDate}</h4>
+        <h4 className={"font-bold"}>
+          Date du cours : {formattedStartDate} - {formattedEndDate}
+        </h4>
       </header>
       <section className={"pt-5"}>
         <h2 className={"pb-10"}>{course.description}</h2>
-        
-        <Table>
-        <TableCaption>A list of your courses to assign</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Select</TableHead>
-            <TableHead>Nom élève</TableHead>
-            <TableHead>E-mail</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {students?.map((student) => {
-            return (
-              <TableRow key={student.id}>
-                <TableCell>
-                
-                </TableCell>
-                <TableCell>{student.nom}</TableCell>
-                <TableCell>{student.email}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-
-
+        <StudentsTable students={students} />
       </section>
       <Link className={"pt-5 w-full flex"} href={"/courses/new"}>
-      <Button>
-        <Label>Modifier</Label>
-      </Button>
-    </Link>
+        <Button>
+          <Label>Modifier</Label>
+        </Button>
+      </Link>
     </div>
   );
 };
